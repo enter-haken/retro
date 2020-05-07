2e67fc889387dbb02bf9162847b3eed783832b16
Jan Frederik Hake <jan_hake@gmx.de>
2020-05-07 13:37:01 +0200

    added site.webmanifest

--- lib/web/plug/bakery.ex
+++ lib/web/plug/bakery.ex
@@ -1,6 +1,6 @@
 defmodule Retro.Web.Plug.Bakery.Fetch do
   defmacro get_files() do
-    static_files = Path.wildcard("client/dist/Retro/**/*.{css,js,ico,html,png,jpg,gz,br}")
+    static_files = Path.wildcard("client/dist/Retro/**/*.{css,js,ico,html,png,jpg,webmanifest}")
 
     static =
       static_files
